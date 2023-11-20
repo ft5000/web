98048d4bb29ce9e9b9b8171257a9570547e5bada
ft5000 <fabian.tjernstrom@gmail.com>
2023-11-20 12:28:45 +0100

    darkmode

---
+++ script.js
@@ -0,0 +1,34 @@
+var text = "$A$b ^biaaaaan.  4$$FAB.F*$$$✞$$✵00000000000000000";
+setInterval(function() {
+    var title = "";
+    for (var i = 0; i < 16; i++) {
+        var r = Math.round(Math.random() * (text.length - 1));
+        title += text[r];
+    }
+    document.title = title;
+}, 60)
+
+var ligh_btn = document.getElementById('light');
+var dark_btn = document.getElementById('dark');
+
+var r = document.querySelector(':root');
+
+function setLightTheme() {
+    r.style.setProperty('--col-3', '#ffffff');
+    r.style.setProperty('--col-2', '#ff9999');
+    r.style.setProperty('--col-1', '#c8d0da');
+}
+
+function setDarkTheme() {
+    r.style.setProperty('--col-3', '#000000');
+    r.style.setProperty('--col-2', '#ffffff');
+    r.style.setProperty('--col-1', '#111111');
+}
+
+ligh_btn.addEventListener('click', function() {
+    setLightTheme()
+})
+
+dark_btn.addEventListener('click', () => {
+    setDarkTheme()
+});
